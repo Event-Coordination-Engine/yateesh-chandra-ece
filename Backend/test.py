34b76a13056756a08da01cbe717d49184cc4f8ad
= <=>
2024-09-15 21:43:24 +0530

    Removed Test file

--- Backend/test.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-i = ["19-09-2024", "01-09-2023", "09-12-2023"]
-# f = datetime.now().strftime("%d-%m-%Y") - 1
-
-# Format it as a string in the desired format (day-month-year)
-date_to_show = (datetime.now() - timedelta(days=2)).strftime("%d-%m-%Y")
-
-print(formatted_yesterday)
-# print(f)
