6e9beddbcb4ccbb8ac4c10fc59d9360db7b393fc
= <=>
2024-09-29 00:36:13 +0530

    Modified frontend according to Coding Standards..!

--- frontend/src/sweetalerts/SweetAlert.js
+++ frontend/src/sweetalerts/SweetAlert.js
@@ -13,7 +13,6 @@ class SweetAlert{
         Swal.fire({
           title : "Successfully Registered",
           text : "Hurray, Now you are our subscriber.!",
-        //   imageUrl : img,
           imageHeight : 150,
           imageWidth : 150,
         })
